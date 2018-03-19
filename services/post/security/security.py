984b23614708071b548705d6f9836c5b3abb490c
Nikolay <nikolay.skliarenko@gmail.com>
2018-03-19 15:10:44 +0300

    Started working on user service

--- services/post/security/security.py
+++ services/post/security/security.py
@@ -1,5 +1,5 @@
 from OpenSSL import SSL
 
-context = SSL.Context(SSL.SSLv23_METHOD)
+context = SSL.Context(SSL.TLSv1_2_METHOD)
 #context.use_privatekey("./post.key")
 #context.use_certificate("./post.crt")
