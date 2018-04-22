cd5a8e7f1024df599d4c8337eb5e42f3ee6ff77f
Nikolay <nikolay.skliarenko@gmail.com>
2018-04-22 17:55:05 +0300

    Changed config for user service

--- services/user/config.py
+++ services/user/config.py
@@ -8,6 +8,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     USER_SERVICE_PATH = "/users"
     USER_URL_PATH = "/<user_id>"
+
+    GATEWAY_URL_PATH = "/gateway/api"
     CHECK_ROLE_URL_PATH = "/auth/check_role"
     GET_TOKEN_URL_PATH = "/auth/token"
     TOKEN_CHECK_ID_URL_PATH = "/auth/token/check_id"
@@ -17,6 +19,8 @@ class DevelopmentConfig(Config):
     DEBUG = True
     PORT = 5001
     USER_SERVICE_URL = "http://127.0.0.1:%d" % PORT
+    GATEWAY_SERVICE_URL = "http://127.0.0.1:5000"
+
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://tester:111@localhost/users_db"
     TOKEN_EXPIRATION_TIME = 2000
 
